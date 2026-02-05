805a7e09db73f3e932c2cb268fd077cd3be3aef1
Hannes <hannes.gingby@hitachigymnasiet.se>
2026-02-05 14:11:49 +0100

    migrate properly to the new treesitter rewrite

--- nvim/init.lua
+++ nvim/init.lua
@@ -703,39 +703,25 @@ require('lazy').setup {
     end,
   },
 
+  -- NOTE: requires tree-sitter-cli
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
-    main = 'nvim-treesitter', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      -- Autoinstall languages that are not installed
-      install = {
-        'svelte',
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'javascript',
-        'typescript',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    config = function()
+      local filetypes =
+        { 'bash', 'c', 'diff', 'html', 'javascript', 'typescript', 'svelte', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+
+      require('nvim-treesitter').install(filetypes)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetypes,
+        callback = function()
+          vim.treesitter.start()
+        end
+      })
+    end,
   },
 
   require 'kickstart.plugins.autopairs',
