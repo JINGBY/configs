6ccbaca51b60655cdd9e808b29b0b50f73ce6636
Hannes <hannes.gingby@hitachigymnasiet.se>
2026-04-20 08:03:36 +0200

    Just symlink instead of using nix

---
+++ symlink.sh
@@ -0,0 +1,19 @@
+##!/usr/bin/env bash
+
+CONFIGS=~/configs
+
+link() {
+  rm -rf "$2"
+  ln -s "$1" "$2"
+}
+
+mkdir -p ~/.config/tmux
+mkdir -p ~/.local/bin
+
+link $CONFIGS/nvim ~/.config/nvim
+link $CONFIGS/ghostty ~/.config/ghostty
+link $CONFIGS/tmux/tmux.conf ~/.config/tmux/tmux.conf
+link $CONFIGS/tmux/tmux-sessionizer ~/.local/bin/tmux-sessionizer
+link $CONFIGS/zshrc ~/.zshrc
+
+chmod +x ~/.local/bin/tmux-sessionizer
