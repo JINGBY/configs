7075f59f871d4b2db51dc7f759a1f964571010d8
Hannes <hannes.gingby@hitachigymnasiet.se>
2026-04-21 10:27:25 +0200

    Fix nvim-treesitter config

--- nvim/lua/hg/plugins/treesitter.lua
+++ nvim/lua/hg/plugins/treesitter.lua
@@ -1,18 +1,37 @@
 return {
-  -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   branch = "main",
   build = ":TSUpdate",
   config = function()
-    local filetypes =
-      { "bash", "c", "diff", "html", "javascript", "typescript", "svelte", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" }
+    require("nvim-treesitter").setup {
+      install_dir = vim.fn.stdpath "data" .. "/site",
+    }
 
+    local filetypes = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "typescript",
+      "svelte",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "vim",
+      "vimdoc",
+    }
     require("nvim-treesitter").install(filetypes)
 
-    require("nvim-treesitter").setup {
-      ensure_installed = filetypes,
-      highlight = { enable = true },
-    }
+    -- Enable highlighting for all the filetypes you care about
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
   end,
 }
